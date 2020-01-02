f473c2cbe3030bf3e9eeadbe92122e5b483b1b82
Makoto Uwaya <makoto.uwaya@gmail.com>
2020-01-02 12:59:35 +0900

    chap #8 Add CharactrerStatus

---
+++ Assets/Scripts/CharacterStatus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CharacterStatus : MonoBehaviour
+{
+    // 体力
+    public int HP = 100;
+    public int MaxHP = 100;
+
+    // 攻撃力
+    public int Power = 10;
+
+    // 最後に攻撃した対象
+    public GameObject lastAttackTarget = null;
+
+    // プレイヤー名.
+    public string characterName = "Player";
+
+    //状態
+    public bool attacking = false;
+    public bool died = false;
+
+}
